e658802f4b38871e8fd9b71d73e79f27e2c1aa33
MRbozik <oleksii.vrublevskyi@gmail.com>
2021-06-23 15:50:47 +0300

    Усовершенствовал программу и добавил вывод

--- task3/task_3.py
+++ task3/task_3.py
@@ -1,40 +1,48 @@
 import http.client
+from tkinter import *
 import re
-#Search inf
-def search_info(key_word, temp):
-    result = re.search(key_word, temp)
-    start = result.start()
-    i = -1
+
+
+# Поиск информации по стране, ключ-слову в tmp
+def search_info(country,tmp,key_word):
+    result_country = re.search(country, tmp)
+    end_country = result_country.end() #поиск начала поиска по стране
+    result_search = re.search(key_word, tmp[end_country:])
+    end_search = result_search.end() # поиск по ключслову
+    s = key_word
     while True:
-        if temp[i+start] == ',':
-            break
-        print(temp[i+start], end='')
-        i += 1
-    return temp[result.end():]
+       # print(end_search)
+        if tmp[end_search+end_country+1] == ":":
+            i=1
+            while tmp[end_search+i+end_country] != ",":
+                s += tmp[end_search+i+end_country]
+                i+=1
+
+            print(s)
+            return s
+        else: break
+
+
 conn = http.client.HTTPSConnection("vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com")
 headers = {
     'x-rapidapi-key': "2c501bdd53msh281d423c869cf15p18f22cjsn36d828e0f50d",
     'x-rapidapi-host': "vaccovid-coronavirus-vaccine-and-treatment-tracker.p.rapidapi.com"
     }
-# Choose Country
-conn.request("GET", "/api/npm-covid-data/africa", headers=headers)
+conn.request("GET", "/api/npm-covid-data/asia", headers=headers)
 res = conn.getresponse()
 data = res.read()
-temp = data.decode("utf-8")
+tmp = data.decode("utf-8")
 print(data.decode("utf-8"))
-result1 = 0
-result1 = re.findall("Country", temp)
-start = temp
-for i in range(len(result1)):
-    start1 = search_info("Country", start)
-    print("-------",i)
-    start2 = search_info("TotalCases", start1)
-    print()
-    start3 =search_info("TotalDeath", start2)
-    print()
-    start4 =search_info("TotalRecovered", start3)
-    print()
-    start5 = search_info("TotalTests",start4)
-    print()
-    start = start5
-
+lst_country = ["India","Turkey","Indonesia","Iraq","Israel"]
+lst_search = ["TotalCases","NewCases","TotalDeaths","NewDeaths","TotalRecovered"]
+#Show
+root = Tk()
+root.title("Covid Asia")
+root.geometry("600x1600")
+for i in range(len(lst_country)):
+    lab_country = Label(root, text = lst_country[i],font ="Arial 14",fg = "black" )
+    lab_country.place(x = 20, y = 50*i*4)
+    for j in range (len(lst_search)):
+        lab_search = Label(root, text = search_info(lst_country[i],tmp,lst_search[j]),font ="Arial 14",fg = "green" )
+        lab_search.place(x = 20, y = 50*i*4+30*(j+1))
+root.mainloop()
